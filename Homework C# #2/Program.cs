a404f5aa8b2abaa7647125cb0bc14299f1404dde
Dauren <daurmegatron@gmail.com>
2022-12-22 02:58:15 +0600

    решил все задачи

---
+++ Homework C# #2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int DataNumber(string msg)
+{
+    Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Decay(int number)
+{
+    int sum;
+    for (sum = 0; number != 0; number = number/10)
+    {
+        int count = number % 10;
+        sum = sum + count;
+    }
+    return sum;
+}
+
+int num = DataNumber("Введите число ");
+if(num > 0)
+{
+    int sum = Decay(num);
+    Console.WriteLine(sum);
+}
+else Console.WriteLine("Введено неверное число");
---
+++ Homework C# #3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int DataSize(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void PrintArray(int[] charr)
+{
+    int i;
+    for (i = 0; i < charr.Length - 1; i++)
+    {
+        Console.Write(charr[i] + ", ");
+    }
+    Console.Write(charr[i]);
+}
+
+void RandomIndex(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+}
+
+Console.Clear();
+int index = DataSize("Укажите размер массива ");
+int[] array = new int[index];
+RandomIndex(array);
+PrintArray(array);
